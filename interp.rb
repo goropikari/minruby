71dd20d933d7f5ec9b014b5d4d84e2ee4d13b274
goropikari <goropikari56@gmail.com>
2020-10-01 21:19:55 +0900

    implement builtin function

--- interp.rb
+++ interp.rb
@@ -1,57 +1,68 @@
 require 'minruby'
 
-env = {}
-env["plus_count"] = 0
-def evaluate(tree, env)
+genv = {"p" => ["builtin", "p"]}
+lenv = {}
+lenv["plus_count"] = 0
+def evaluate(tree, genv, lenv)
   case tree[0]
   when "lit"
     tree[1]
   when "+"
-    env["plus_count"] += 1
-    evaluate(tree[1], env) + evaluate(tree[2], env)
+    lenv["plus_count"] += 1
+    evaluate(tree[1], genv, lenv) + evaluate(tree[2], genv, lenv)
   when "-"
-    evaluate(tree[1], env) - evaluate(tree[2], env)
+    evaluate(tree[1], genv, lenv) - evaluate(tree[2], genv, lenv)
   when "*"
-    evaluate(tree[1], env) * evaluate(tree[2], env)
+    evaluate(tree[1], genv, lenv) * evaluate(tree[2], genv, lenv)
   when "/"
-    evaluate(tree[1], env) / evaluate(tree[2], env)
+    evaluate(tree[1], genv, lenv) / evaluate(tree[2], genv, lenv)
   when "%"
-    evaluate(tree[1], env) % evaluate(tree[2], env)
+    evaluate(tree[1], genv, lenv) % evaluate(tree[2], genv, lenv)
   when "**"
-    evaluate(tree[1], env) ** evaluate(tree[2], env)
+    evaluate(tree[1], genv, lenv) ** evaluate(tree[2], genv, lenv)
   when "=="
-    evaluate(tree[1], env) == evaluate(tree[2], env)
+    evaluate(tree[1], genv, lenv) == evaluate(tree[2], genv, lenv)
   when ">"
-    evaluate(tree[1], env) > evaluate(tree[2], env)
+    evaluate(tree[1], genv, lenv) > evaluate(tree[2], genv, lenv)
   when "<"
-    evaluate(tree[1], env) < evaluate(tree[2], env)
+    evaluate(tree[1], genv, lenv) < evaluate(tree[2], genv, lenv)
   when "func_call"
-    p(evaluate(tree[2], env))
+    args = []
+    i = 0
+    while tree[i+2]
+      args[i] = evaluate(tree[i+2], genv, lenv)
+      i = i + 1
+    end
+    mhd = genv[tree[1]]
+    if mhd[0] == "builtin"
+      minruby_call(mhd[1], args)
+    else
+    end
   when "stmts"
     i = 1
     while tree[i] != nil
-      last = evaluate(tree[i], env)
+      last = evaluate(tree[i], genv, lenv)
       i = i + 1
     end
     last
   when "var_assign"
-    env[tree[1]] =  evaluate(tree[2], env)
+    lenv[tree[1]] =  evaluate(tree[2], genv, lenv)
   when "var_ref"
-    env[tree[1]]
+    lenv[tree[1]]
   when "if"
-    if (evaluate(tree[1], env))
-      evaluate(tree[2], env)
+    if (evaluate(tree[1], genv, lenv))
+      evaluate(tree[2], genv, lenv)
     else
-      evaluate(tree[3], env)
+      evaluate(tree[3], genv, lenv)
     end
   when "while"
-    while evaluate(tree[1], env)
-      evaluate(tree[2], env)
+    while evaluate(tree[1], genv, lenv)
+      evaluate(tree[2], genv, lenv)
     end
   when "while2"
-    evaluate(tree[2], env)
-    while evaluate(tree[1], env)
-      evaluate(tree[2], env)
+    evaluate(tree[2], genv, lenv)
+    while evaluate(tree[1], genv, lenv)
+      evaluate(tree[2], genv, lenv)
     end
   else
     p("No method matching")
@@ -75,4 +86,4 @@ end
 
 str = minruby_load()
 tree = minruby_parse(str)
-evaluate(tree, env)
+evaluate(tree, genv, lenv)
